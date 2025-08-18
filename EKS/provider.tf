1ce47fdc635fdb9b142319c3093183e607440fe2
rajasekhar82 <rajasekharn82@gmail.com>
2025-08-18 11:24:29 +0530

    update account values

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.0"   # latest stable that supports your EKS module
+    }
+  }
+
+  required_version = ">= 1.3"
+}
+
 provider "aws" {
   region = "us-east-2"
 }
