e23336a3c3822c25acb7c511ca374e0d6835be27
rajasekhar82 <rajasekharn82@gmail.com>
2025-08-18 11:34:19 +0530

    update account values

--- EKS/provider.tf
+++ EKS/provider.tf
@@ -2,11 +2,10 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 6.0.0"
+      version = ">= 5.40.0" # ensure >= 5.x, supports GPU & inference
     }
   }
 }
-
 
 provider "aws" {
   region = "us-east-2"
