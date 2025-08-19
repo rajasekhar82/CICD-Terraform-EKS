765f4768565ea603427c8dbf2acdeceb3ab9accd
rajasekhar82 <rajasekharn82@gmail.com>
2025-08-19 22:48:09 +0530

    update EKS files

--- EKS/main.tf
+++ EKS/main.tf
@@ -73,4 +73,11 @@ data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
 
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.this.endpoint
+  token                  = data.aws_eks_cluster_auth.this.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
+}
+
+
 
