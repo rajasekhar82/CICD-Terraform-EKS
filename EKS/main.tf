cff0eac3820214eca448985717d31276e8b3f59f
rajasekhar82 <rajasekharn82@gmail.com>
2025-08-19 22:39:42 +0530

    update EKS files

--- EKS/main.tf
+++ EKS/main.tf
@@ -37,23 +37,22 @@ module "vpc" {
 # -------------------------------
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "21.0.5"
+  version = "~> 21.0"
 
-  eks_cluster {
-    name     = "my-eks-cluster"
-    version  = "1.33"
-    endpoint_public_access = true
-  }
+  name               = "my-eks-cluster"
+  kubernetes_version = "1.33"
+  endpoint_public_access = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   eks_managed_node_groups = {
     default = {
+      ami_type       = "AL2023_x86_64_STANDARD"
+      instance_types = var.instance_types
       min_size       = 1
       max_size       = 3
       desired_size   = 2
-      instance_types = var.instance_types
     }
   }
 
