b8ea6f75774412de42487eb6dbb67265ea12a656
rajasekhar82 <rajasekharn82@gmail.com>
2025-08-19 22:32:24 +0530

    update EKS files

--- EKS/main.tf
+++ EKS/main.tf
@@ -39,9 +39,11 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "21.0.5"
 
-  cluster_name                   = "my-eks-cluster"
-  cluster_version                = "1.33"
-  cluster_endpoint_public_access = true
+  eks_cluster {
+    name     = "my-eks-cluster"
+    version  = "1.33"
+    endpoint_public_access = true
+  }
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
