ef88e42c176900f22051285bc2c7a23b7adc9dfb
rajasekhar82 <rajasekharn82@gmail.com>
2025-08-19 22:41:46 +0530

    update EKS files

--- EKS/main.tf
+++ EKS/main.tf
@@ -73,17 +73,4 @@ data "aws_eks_cluster_auth" "this" {
   name = module.eks.cluster_name
 }
 
-# -------------------------------
-# Outputs
-# -------------------------------
-output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
-}
 
-output "cluster_name" {
-  value = module.eks.cluster_name
-}
-
-output "vpc_id" {
-  value = module.vpc.vpc_id
-}
